f318eed0c166e48f20c9b49cf1ae2207a4880366
kevincrys <kevin.c.costa@edu.unirio.br>
2023-06-15 14:34:29 -0300

    dados invalidos

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-import { ValidationPipe } from '@nestjs/common';
+import {  UnprocessableEntityException, ValidationError, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors: ValidationError[]) => 
+      new UnprocessableEntityException('Dados Inválidos'),
+  }));
   await app.listen(3000);
 }
 bootstrap();
