8fef40650fc803ca0d9d4ee09c86ac58c2e5174d
kevin.costa.glo <kevin.costa@globalhitss.com.br>
2023-06-19 16:52:17 -0300

    corrigindo imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CiclistaModule } from './modules/ciclista/ciclista.module';
 import { FuncionarioModule } from './modules/funcionario/funcionario.module';
 import { CartaoModule } from './modules/cartao/cartao.module';
@@ -8,6 +7,6 @@ import { CartaoModule } from './modules/cartao/cartao.module';
 @Module({
   imports: [CiclistaModule,FuncionarioModule,CartaoModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
